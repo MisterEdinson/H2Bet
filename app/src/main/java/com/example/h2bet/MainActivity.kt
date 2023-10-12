493ab8239a635fa340f9e5ebabbaeb8c83976620
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-10-12 12:34:48 +0500

    add game ui tic toc toe and spin

--- app/src/main/java/com/example/h2bet/MainActivity.kt
+++ app/src/main/java/com/example/h2bet/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.h2bet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.h2bet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
